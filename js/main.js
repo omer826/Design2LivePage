03694be8c37ce73ba9f3a690b56e691a1df6ae44
omers <omer3026@gmail.com>
2018-06-17 18:15:08 +0300

    style chnages

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ function init() {
 
 function toggleMenu(elBtn) {
     document.querySelector('.main-menu').classList.toggle('open');
-    elBtn.classList.toggle('fa-window-close');
-    // fa-window-close
+    elBtn.classList.toggle('fa-bars');
+    elBtn.classList.toggle('fa-close');
 }
 
 function createSubMenu() {
@@ -21,34 +21,42 @@ function renderSubMenus(menus) {
     var elAMenuItem = document.querySelector('.sub-menu');
     var strHtml = ``;
     menus.forEach(function (item) {
-        strHtml += `<li class="sub-menu-item flex column space-around align-items"> ${item}  </li>`
+        strHtml += `
+        <li >
+        <a  class="sub-menu-item flex column space-around align-items" href="#"> ${item} </a>
+         </li>`
     });
     elAMenuItem.innerHTML = strHtml;
 }
 
-function openSubMenu() {
-    var elSubMenu = document.querySelector('.sub-menu');
-    elSubMenu.classList.add('openSubMenu');
-}
-
-function closeSubMenu() {
-    var elSubMenu = document.querySelector('.sub-menu');
-    elSubMenu.classList.remove('openSubMenu');
-}
-
 function toggleSubMenu() {
     var elSubMenu = document.querySelector('.sub-menu');
-    elSubMenu.classList.toggle('openSubMenu');
+    if(elSubMenu.style.visibility === 'hidden'){
+        elSubMenu.classList.toggle('openSubMenu');
+
+    }
 }
 
 function readMore(el) {
     var elModal = document.querySelector('.modal');
+    var elInforModal = document.querySelector('.modal-info');
     elModal.classList.add('open-modal');
+    elInforModal.classList.add('modal-info-transform');
 }
 
 function onSubmit(elBtn) {
+    event.stopPropagation();
     var elModal = document.querySelector('.modal');
     var elInputEmailVal = document.querySelector('.input-email').value;
 
+    // elModal.classList.remove('open-modal');
+}
+
+function closeModal(ev){
+    var elModal = document.querySelector('.modal');
+    var elInforModal = document.querySelector('.modal-info');
     elModal.classList.remove('open-modal');
+    elInforModal.classList.remove('modal-info-transform');
+
+    
 }
